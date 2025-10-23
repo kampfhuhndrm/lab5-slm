5cedc37059f2bf4b55d6d88da1d3d975a67d5125
David Mayer <davidmayer@eduroam-10-200-9-179.local.su.se>
2025-10-23 14:34:05 +0200

    started with KGramcpp

--- src/KGram.cpp
+++ src/KGram.cpp
@@ -1,3 +1,38 @@
 #include <string>
 #include <iostream>
 #include "KGram.h"
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+KGram::KGram(const string& filename, int k)
+{
+    k_ = k;
+    std::ifstream file(filename);
+    if(!file.is_open()){
+        throw std::runtime_error(filename+"couldnt be opened");
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    string content = buffer.str();
+
+    file.close();
+
+    if(content.length() <= k_){
+        return;
+    }
+
+    for(size_t i=0; i < content.length() - k_;i++){
+        string kgram = content.substr(i, k_);
+
+        char next_char = content[i + k_];
+
+        freq_[kgram]++;
+        transitions_[kgram][next_char]++;
+        total_count_++;
+    }
+
+}
+
+float KGram
